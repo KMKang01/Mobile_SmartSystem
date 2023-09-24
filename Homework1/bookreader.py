6ab1945d28232657cacaad3558a60a74f2f15d12
kmk01 <wowkmini15@gmail.com>
2023-09-24 22:42:59 +0900

    Update bookreader.py

--- Homework1/bookreader.py
+++ Homework1/bookreader.py
@@ -7,10 +7,11 @@ for line in file.readlines():
         한 줄씩 읽어서 line 변수에 저장
         for 문은 phonebook.txt의 모든 내용을 읽을 때까지 반복
         '''
-        aline = line.strip() 
+        tmp = line.split(",",1)
+        aline = "이름은 " + tmp[0] + ", 전화번호는 " + tmp[1]
         '''
-        line 변수에 포함되는 줄바꿈 문자를
-        strip 함수로 삭제한 후 aline 변수에 저장
+        tmp로 line에 저장된 문자열을 콤마를 기준으로 나눠서
+        각각 tmp[0], tmp[1]에 저장 후 aline 변수에 문자열로 저장
         '''
-        print(aline) # aline 변수를 출력
+        print(aline.strip()) # aline 변수를 strip 함수로 줄 바꿈을 지우고 출력
 file.close()
